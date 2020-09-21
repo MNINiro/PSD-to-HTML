bc8773c019398c0338be51c5ac13579300f70776
MNI Niro <mniniro@hotmail.com>
2020-09-21 11:14:58 +0600

    3rd Commit

---
+++ Part10/resources/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    var mixer = mixitup('.container');
+});
+
+/* it initially load the index.html file then it will load the js file */
+  /* it will connect with .container class of the portfolio section */
+ 
